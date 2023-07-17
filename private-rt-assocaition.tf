8507cbb10597c7daf0cd5a4a396528616b8195db
sunday <sundayoyeniyi33@gmail.com>
2023-07-17 05:01:47 +0100

    deleted terraform

--- private-rt-assocaition.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_route_table_association" "terraform-private-subnet-1-association" {
-  subnet_id      = aws_subnet.terraform-private-subnet-1.id
-  route_table_id = aws_route_table.terraform-private-rt-1.id
-}
-
-resource "aws_route_table_association" "terraform-private-subnet-2-association" {
-  subnet_id      = aws_subnet.terraform-private-subnet-2.id
-  route_table_id = aws_route_table.terraform-private-rt-2.id
-}
--- public-rt-associatiom.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_route_table_association" "terraform-public-subnet-1-association" {
-  subnet_id      = aws_subnet.terraform-public-subnet-1.id
-  route_table_id = aws_route_table.terraform-public-rt.id
-}
-
-resource "aws_route_table_association" "terraform-public-subnet-2-association" {
-  subnet_id      = aws_subnet.terraform-public-subnet-2.id
-  route_table_id = aws_route_table.terraform-public-rt.id
-}
